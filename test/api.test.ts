22600fbe0b3d314eca307effc1e0cd146cbb6a04
sancty <sanctifieryawmensah55@gmail.com>
2025-09-23 18:10:43 +0100

    Uncomment API test cases for the GET /api/v1 endpoint to enable testing and ensure proper functionality.

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,18 +1,18 @@
-// import request from "supertest";
-// import { describe, it } from "vitest";
+import request from "supertest";
+import { describe, it } from "vitest";
 
-// import app from "../src/app.js";
+import app from "../src/app.js";
 
-// describe("GET /api/v1", () => {
-//   it("responds with a json message", () =>
-//     request(app)
-//       .get("/api/v1")
-//       .set("Accept", "application/json")
-//       .expect("Content-Type", /json/)
-//       .expect(200, {
-//         message: "API - 👋🌎🌍🌏",
-//       }));
-// });
+describe("GET /api/v1", () => {
+  it("responds with a json message", () =>
+    request(app)
+      .get("/api/v1")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, {
+        message: "API - 👋🌎🌍🌏",
+      }));
+});
 
 // describe("GET /api/v1/emojis", () => {
 //   it("responds with a json message", () =>
