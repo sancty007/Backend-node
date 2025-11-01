3e2d730eaafdbdf5a6beabf2bcab187898438974
sancty <sanctifieryawmensah55@gmail.com>
2025-11-01 20:19:09 +0100

    refactor: delete text in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,18 @@ NAME="pokemon-api"
 USERNAME="jupsy"
 IMAGE="$USERNAME/$NAME:latest"
 
-echo "🚀 Building Docker image..."
+echo "Building Docker image..."
 docker build -t $IMAGE .
 
-echo "📤 Pushing image to Docker Hub..."
+echo "Pushing image to Docker Hub..."
 docker push $IMAGE
 
-echo "🧩 Applying Kubernetes manifests..."
+echo "Applying Kubernetes manifests..."
 kubectl apply -f k8s/deployment.yaml
 kubectl apply -f k8s/service.yaml
 
-echo "🐳 Getting pods..."
+echo "Getting pods..."
 kubectl get pods
 
-echo "🌐 Getting services..."
+echo "Getting services..."
 kubectl get services
