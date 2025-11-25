852e22835245aa062a4815accffe6f34b30edfd2
sancty <sanctifieryawmensah55@gmail.com>
2025-11-25 08:07:38 +0100

    feat: integrate Prometheus monitoring and Grafana deployment in deploy.sh; add prom-client for metrics tracking in app.ts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 NAME="pokemon-api"
@@ -15,8 +14,32 @@ echo "Applying Kubernetes manifests..."
 kubectl apply -f k8s/deployment.yaml
 kubectl apply -f k8s/service.yaml
 
-echo "Getting pods..."
-kubectl get pods
+# === Prometheus / Grafana ===
 
-echo "Getting services...."
-kubectl get services
+echo "Creating monitoring namespace..."
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Applying Prometheus config..."
+kubectl apply -f monitoring/prometheus/prometheus-config.yaml
+
+echo "Deploying Prometheus..."
+kubectl apply -f monitoring/prometheus/prometheus-deployment.yaml
+kubectl apply -f monitoring/prometheus/prometheus-service.yaml
+
+echo "Deploying Grafana..."
+kubectl apply -f monitoring/grafana/grafana-deployment.yaml
+kubectl apply -f monitoring/grafana/grafana-service.yaml
+
+echo "Waiting 10s for pods to start..."
+sleep 10
+
+echo "Getting monitoring pods..."
+kubectl get pods -n monitoring
+
+echo "Getting monitoring services..."
+kubectl get svc -n monitoring
+
+echo ""
+echo "To access Grafana:"
+echo "kubectl port-forward svc/grafana 3001:3000 -n monitoring"
+echo "Then open: http://localhost:3001"
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import client from "prom-client";
 
 import type { MessageResponse } from "./types/index.js";
 
@@ -14,6 +15,30 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+const register = new client.Registry();
+
+// Compteur pour les requêtes HTTP
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Nombre total de requêtes HTTP",
+  labelNames: ["method", "route", "statusCode"],
+});
+register.registerMetric(httpRequestCounter);
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestCounter
+      .labels(req.method, req.path, res.statusCode.toString())
+      .inc();
+  });
+  next();
+});
+
+// Endpoint pour Prometheus
+app.get("/metrics", async (_req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get<object, MessageResponse>("/", (req, res) => {
   res.json({
